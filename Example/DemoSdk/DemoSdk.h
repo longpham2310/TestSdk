92b3f651a94b4249e5cccb9e6b17d216897f1968
Long Pham <philong.bmt@gmail.com>
2024-03-15 10:33:17 +0700

    add demo sdk framework

---
+++ Example/DemoSdk/DemoSdk.h
@@ -0,0 +1,18 @@
+//
+//  DemoSdk.h
+//  DemoSdk
+//
+//  Created by Long Pham on 15/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoSdk.
+FOUNDATION_EXPORT double DemoSdkVersionNumber;
+
+//! Project version string for DemoSdk.
+FOUNDATION_EXPORT const unsigned char DemoSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoSdk/PublicHeader.h>
+
+
---
+++ Example/DemoSdk/DemoSdk.swift
@@ -0,0 +1,15 @@
+//
+//  DemoSdk.swift
+//  DemoSdk
+//
+//  Created by Long Pham on 15/03/2024.
+//
+
+import Foundation
+import ZIPFoundation
+
+struct DemoSdk {
+    func hello() {
+        print("Hello")
+    }
+}
