a8dbb0f91efa7ad49518c877b66869506ea13885
Long Pham <philong.bmt@gmail.com>
2024-06-10 15:37:05 +0700

    test

---
+++ Frameworks/self_mobile_kmp.framework/Frameworks/libomemo.framework/Headers/OmemoWrapper.h
@@ -0,0 +1,16 @@
+//
+//  OmemoWrapper.h
+//  self-ios
+//
+//  Created by Long Pham on 11/01/2021.
+//  Copyright © 2021 Aldgate Ventures. All rights reserved.
+//
+
+#ifndef OmemoWrapper_h
+#define OmemoWrapper_h
+
+//#import "GroupSessionWrapper.h"
+//#import "OlmAccountWrapper.h"
+//#import "OlmSessionWrapper.h"
+
+#endif /* OmemoWrapper_h */
---
+++ Frameworks/self_mobile_kmp.framework/Frameworks/libomemo.framework/Headers/libomemo.h
@@ -0,0 +1,26 @@
+//
+//  libomemo.h
+//  libomemo
+//
+//  Created by Long Pham on 28/06/2023.
+//  Copyright © 2023 Join Self. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libomemo.
+FOUNDATION_EXPORT double libomemoVersionNumber;
+
+//! Project version string for libomemo.
+FOUNDATION_EXPORT const unsigned char libomemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libomemo/PublicHeader.h>
+#import <libomemo/OmemoWrapper.h>
+#import <libomemo/GroupSessionWrapper.h>
+#import <libomemo/OlmAccountWrapper.h>
+#import <libomemo/OlmSessionWrapper.h>
+//#import "GroupSessionWrapper.h"
+//#import "OlmAccountWrapper.h"
+//#import "OlmSessionWrapper.h"
+
+
